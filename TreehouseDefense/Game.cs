6406a0ffce084a96dedae3e30b5bff096d97c7d4
JLyons009 <JLyons009@gmail.com>
2017-04-06 16:06:16 -0500

    Added MapLocation class, a child class to Point

--- TreehouseDefense/Game.cs
+++ TreehouseDefense/Game.cs
@@ -13,10 +13,11 @@ namespace TreehouseDefense
             Map map = new Map(8, 5);
 
             Point point = new Point(4, 2);
-            Point point2 = new Point(5, 5);
 
-            Console.WriteLine(point.DistanceTo(5, 5));
-            Console.WriteLine(point.DistanceTo(point2));
+            MapLocation mapL = new MapLocation(5, 5);
+
+            Console.WriteLine(mapL is Point);
+            Console.WriteLine(mapL is MapLocation);
         }
     }
 }
