adc62fddae033dbc38442c4c17b4b5be5223523a
JLyons009 <JLyons009@gmail.com>
2017-04-06 11:52:18 -0500

    Added constructor for Map object

--- TreehouseDefense/Map.cs
+++ TreehouseDefense/Map.cs
@@ -8,7 +8,13 @@ namespace TreehouseDefense
 {
     class Map
     {
-        public int Width;
-        public int Height;
+        public readonly int Width;
+        public readonly int Height;
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
     }
 }
