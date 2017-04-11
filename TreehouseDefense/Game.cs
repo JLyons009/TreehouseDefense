904bd62f752c3563c0a0d86f8c033f21b6781278
JLyons009 <JLyons009@gmail.com>
2017-04-11 15:37:24 -0500

    Added Path class as well as GetLocationAt method that checks to see if location is on the path

--- TreehouseDefense/Game.cs
+++ TreehouseDefense/Game.cs
@@ -14,7 +14,25 @@ namespace TreehouseDefense
 
             try
             {
-                MapLocation mapLocation = new MapLocation(20, 20, map);
+                Path path = new Path(
+                    new [] {
+                        new MapLocation(0, 2, map),
+                        new MapLocation(1, 2, map),
+                        new MapLocation(2, 2, map),
+                        new MapLocation(3, 2, map),
+                        new MapLocation(4, 2, map),
+                        new MapLocation(5, 2, map),
+                        new MapLocation(6, 2, map),
+                        new MapLocation(7, 2, map),
+                    }
+                );
+
+                MapLocation location = path.GetLocationAt(0);
+
+                if (location != null)
+                {
+                    Console.WriteLine(location.X + "," + location.Y);
+                }
             }
             catch (OutOfBoundsException ex)
             {
@@ -24,9 +42,9 @@ namespace TreehouseDefense
             {
                 Console.WriteLine("Unhandled TreehouseDefenseException");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                Console.WriteLine("Unhandled Exception");
+                Console.WriteLine("Unhandled Exception" + ex);
             }
         }
     }
