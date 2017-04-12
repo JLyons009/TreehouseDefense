72909d59275ecb16b2fac542b7e1655a0e1a14f4
JLyons009 <JLyons009@gmail.com>
2017-04-12 16:04:18 -0500

    Added getter for Path class's length.  Added Location, Health, and HasScored Properties for Invader.  Added Move() and Hit(int) methods for Invader.

--- TreehouseDefense/Invader.cs
+++ TreehouseDefense/Invader.cs
@@ -8,6 +8,19 @@ namespace TreehouseDefense
 {
     class Invader
     {
-        public MapLocation Location { get; private set; }
+        private readonly Path _path;
+        private int _pathStep = 0;
+       
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
+        public int Health { get; private set; } = 2;
+        public bool HasScored => (_pathStep >= _path.Length);
+
+        public Invader(Path path)
+        {
+            _path = path;
+        }
+
+        public void Move() => _pathStep++;
+        public void Hit(int factor) => Health -= factor;
     }
 }
